2a972b050c32aae8329a60b839aa50e63d9474c9
edykab <kaboweiedmond@gmail.com>
2024-05-06 19:06:10 +0100

    Initial commit

---
+++ data_types.py
@@ -0,0 +1,18 @@
+# data_types.py
+integer = 42
+float_number = 3.14
+boolean = True
+string = "Hello, World!"
+list = [1, 2, 3]
+tuple = (1, 2, 3)
+dictionary = {"name": "John", "age": 30}
+set = {1, 2, 3}
+
+print(f"The value of integer is {integer}, and the data type is {type(integer)}")
+print(f"The value of float_number is {float_number}, and the data type is {type(float_number)}")
+print(f"The value of boolean is {boolean}, and the data type is {type(boolean)}")
+print(f"The value of string is {string}, and the data type is {type(string)}")
+print(f"The value of list is {list}, and the data type is {type(list)}")
+print(f"The value of tuple is {tuple}, and the data type is {type(tuple)}")
+print(f"The value of dictionary is {dictionary}, and the data type is {type(dictionary)}")
+print(f"The value of set is {set}, and the data type is {type(set)}")
---
+++ full_name.py
@@ -0,0 +1,7 @@
+# full_name.py
+first_name = "Edmond"
+middle_name = "Ifeanyi"
+last_name = "Kabowei"
+
+full_name = f"{first_name} {middle_name} {last_name}"
+print(full_name)
